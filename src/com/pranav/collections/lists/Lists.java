e7e7b65ce93fde62773876389e03b27165cb527b
Pranavgaikwad <pranavgaikwad847@gmail.com>
2025-12-22 23:32:15 +0530

    List and ArrayList IN java Collection frame work

---
+++ src/com/pranav/collections/lists/Lists.java
@@ -0,0 +1,12 @@
+package com.pranav.collections.lists;
+
+public class Lists {
+    public static void main(String[] args) {
+        // An ordered collection aka sequnece
+        // Allows dupliactes
+        // Not fixed in size like arrays
+        // Fast for data retrivals
+        // Various Implementations such as
+        // ArrayList,Stack,Vector,Others
+    }
+}
